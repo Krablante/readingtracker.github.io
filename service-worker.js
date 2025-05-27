6ac38ae4aa5b84dc66cb2af10852052a925e1cce
G626 <103378730+Krablante@users.noreply.github.com>
2025-05-27 19:17:54 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,25 +9,21 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   const requestUrl = new URL(event.request.url);
-
-  // 1) Игнорируем запросы к Firestore и Google Identity APIs
   if (
     requestUrl.origin.includes('firestore.googleapis.com') ||
     requestUrl.origin.includes('identitytoolkit.googleapis.com') ||
     requestUrl.pathname.startsWith('/__/')
   ) {
-    return; // пропускаем эти запросы напрямую
+    return;
   }
 
-  // 2) Не кешируем index.html и корневой запрос — всегда свежая версия
   if (
     requestUrl.origin === location.origin &&
     (requestUrl.pathname === '/' || requestUrl.pathname.endsWith('/index.html'))
   ) {
-    return; // браузер сам выполнит запрос к сети
+    return;
   }
 
-  // 3) Cache-first для прочих ресурсов
   event.respondWith(
     caches.match(event.request).then(cached => {
       return cached || fetch(event.request);
